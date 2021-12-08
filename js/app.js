e6e855c39d671b8c267a5f524527ccaf1435a0b7
YuriPurcote <87339208+YuriPurcote@users.noreply.github.com>
2021-12-08 13:42:32 -0300

    add

--- js/app.js
+++ js/app.js
@@ -5,25 +5,24 @@ $(document).ready(function() {
         event.preventDefault();
 
         var search =$("#search").val(); 
+        
         imagesearch()
         
     })
     function imagesearch() {
+        var porPagina = $("#quantidadePorPagina").val();
+        var numeroPagina = $("#numeroPagina").val();
         $.ajax({
             method:'GET',beforeSend: function (xhr) {
                 xhr.setRequestHeader ("Authorization", api_key);
             },
-            url:"https://api.pexels.com/v1/search?query="+search+"&per_page=33&page=1",
+            url:"https://api.pexels.com/v1/search?query="+search+"&per_page="+porPagina+"&page="+numeroPagina+"",
             success: function (data){
-                console.log(data);
                 data.photos.forEach(photo => {
                     image = `
                         <img src="${photo.src.original}"width="300" height="500" style="margin-bottom:5px;"  class=" rounded "/>
-
-                    
                     `
                     $("#images").append(image)
-                    
                 });
             },
             error: function (error) {
